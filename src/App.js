d992f3a53828f51f28d7f1b8f7a6b565a9976b4c
onanuviie <nuviesilvet@gmail.com>
2023-03-21 10:49:56 +0100

    styled the calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import "./styles.css"
+
 
 function App() {
   return (
-    <div className="clculator-grid">
+    <div className="calculator-grid">
       <div className="output">
-        <div className="previos-operand"></div>
+        <div className="previous-operand">133,567 *</div>
+        <div className="current-operand">133,567</div>
       </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   )
 }
