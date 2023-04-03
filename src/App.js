3c116a33dd2bd1fc4bb526b672932f925716d0a9
onanuviie <nuviesilvet@gmail.com>
2023-04-03 08:39:43 +0100

    formatted the numbers

--- src/App.js
+++ src/App.js
@@ -126,6 +126,19 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split(".")
+
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
@@ -136,9 +149,9 @@ function App() {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-          {previousOperand} {operation}
+        {formatOperand(previousOperand)} {operation}
         </div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" 
       onClick={() => dispatch({ type : ACTIONS.CLEAR})}>
